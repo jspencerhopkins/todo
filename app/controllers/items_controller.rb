d5dcec4826cadac8bacfe8566ce6deab6e787ad4
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-06 13:28:58 -0500

    Create (no display) new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+
+
+	def index
+		@items = Item.new
+	end
+
+	def create
+		@item = Item.create(params.require(:item).permit(:description))
+		redirect_to root_url
+	end
+
 end
